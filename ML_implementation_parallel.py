4d2764b7204cb9afbf1e77c8f4d9073c6ec1c48d
John Tsang <wk3tsang@uwaterloo.ca>
2017-10-17 09:25:48 -0400

    Update ML_implementation_parallel.py

--- ML_implementation_parallel.py
+++ ML_implementation_parallel.py
@@ -1,24 +1,29 @@
+# Purpose: 1) Implement hyperparameter selection/re-selection and 
+#             rolling forecast with parallel programming using 
+#             https://github.com/johntwk/Python-ML-rolling-grid-search
+#          2) Store the actual exchange rates, the predicted 
+#             exchange rates, the score of the forecasts,
+#             hyperparameters, labels and features as .json file
+#             and .csv file
+# Author : John Tsang
+# Date   : October 17, 2017
+
+#Libraries
 from rolling_forecast import rolling_grid_search_ML
 import pandas as pd
 import numpy as np
-from pandas import *
-from numpy import *
-from sklearn import svm
-from sklearn.model_selection import TimeSeriesSplit,ParameterGrid
-from sklearn.neighbors import KNeighborsRegressor
-from sklearn.tree import DecisionTreeRegressor
-from sklearn import svm
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.ensemble import GradientBoostingRegressor
 from joblib import Parallel, delayed
-import collections
 
+#Scoring Function
 def rmse(actual_lst,pred_lst):
     sum_sq_error = 0
     len_lst = len(actual_lst)
     for actual,pred in zip(actual_lst,pred_lst):
         sum_sq_error += (actual - pred)**2
     return (sum_sq_error/len_lst)**(0.5)
+
+# Criterion Function
 def crit_min(score_lst):
     min_val = score_lst[0]
     min_index = 0
@@ -31,18 +36,21 @@ def crit_min(score_lst):
     return (min_index,min_val)
 
 if __name__ == '__main__':
-    data = pd.read_csv("../../../data/all_data.csv")
+    # Reading data
+    data = pd.read_csv("all_data.csv")
     data.set_index("DATE", inplace = True)
     var_lst = data.columns.tolist()
     for var in var_lst:
         data[var] = data[var].astype(float)
     var_lst = data.columns.tolist()
+    # Generate lagged variables (lag order: 1)
     num_lags = 1
     for var in var_lst:
         data[var] = data[var].astype(float)
         for lag in range(1,num_lags + 1):
             col_name = "L"+str(lag)+"."+str(var)
             data[col_name] = data[var].shift(-lag)
+    # Drop missing observations
     data.dropna(axis=0, how='any', inplace = True)
     
     # Generate a list of labels and features
@@ -57,20 +65,19 @@ if __name__ == '__main__':
     predictor_lst = predictor_lst[1:]
     label_lst = endog_lst
     
-    model_lst = [DecisionTreeRegressor(random_state=0)]
+    # Specify the machine learning model and a set of 
+    # hyperparameters for gird search
+    model_lst = [RandomForestRegressor(random_state=0)]
     param_lst = [{'max_depth': [1,2,3,4]}]
     
-    #r = Parallel(n_jobs=2)(delayed(sqrt)(i ** 2) for i in range(10))
-    #
-    #######################
+    # Generate a list of all possible label-feature combinations
     label_feature_pair = []
     for label in label_lst:
         for predictor in predictor_lst:
             label_feature_pair.append([label,predictor])
     
-    print label_feature_pair
-    ###############
-    
+    # Run hyperparameter tuning and rolling forecast with
+    # parallel programming
     r_lst = Parallel(n_jobs=4)(delayed(rolling_grid_search_ML)
                            (model = model_lst[0], y = DataFrame(data[pair[0]]), X = data[pair[1]],
                                            group_size = 365, param_grid=param_lst[0], scoring = rmse, 
@@ -79,6 +86,7 @@ if __name__ == '__main__':
     
     print "\nRolling Forecasts Done!\n"
     
+    # Output data in json and csv
     print "Organizing Json......"
     new_r_lst = []
     for r,pair in zip(r_lst,label_feature_pair):
@@ -89,10 +97,10 @@ if __name__ == '__main__':
                           "label" : pair[0],
                           "feature":pair[1]})     
     result_df = DataFrame(new_r_lst)[['label','feature','score']]
-    result_df.to_csv(path_or_buf="CART_result.csv")
+    result_df.to_csv(path_or_buf="RFR_result.csv")
     # Output json to store results
     import json
     output = {"model": str(model_lst[0].__class__),
               "data" : new_r_lst}
-    with open('json/ML_CART.json','w') as fp:
+    with open('ML_RFR.json','w') as fp:
         json.dump(output,fp)
